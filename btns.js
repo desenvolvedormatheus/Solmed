b9e2be504fed0916ef29dd2034577ac6393f75b8
Matheus Pereira de Souza <109758729+desenvolvedormatheus@users.noreply.github.com>
2022-11-24 12:42:09 -0300

    RE 1.0
    Header adicionado

---
+++ btns.js
@@ -0,0 +1,30 @@
+function abrir_menubar(){
+    let btn0 = document.getElementsByClassName('fechar-menu')[0]
+    let btn1 = document.getElementsByClassName('fechar-menu')[1]
+    let btn2 = document.getElementsByClassName('fechar-menu')[2]
+    let menu = document.querySelector('.menu-header')
+
+    let aux = window.getComputedStyle(menu).right
+
+    if(aux == '-180px'){
+        btn0.style.position = 'absolute'
+        btn0.style.transform = 'rotate(45deg)'
+        
+        btn1.style.display = 'none'
+        
+        btn2.style.position = 'absolute'
+        btn2.style.transform = 'rotate(-45deg)'
+    
+        menu.style.right = '1.7rem'
+    } else{
+        btn0.style.position = 'initial'
+        btn0.style.transform = 'rotate(0deg)'
+        
+        btn1.style.display = 'initial'
+        
+        btn2.style.position = 'initial'
+        btn2.style.transform = 'rotate(0deg)'
+    
+        menu.style.right = '-9rem'
+    }
+}
